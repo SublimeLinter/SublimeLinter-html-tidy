3da0310658b0c13ca2854585d81efbf846f495e3
Koen Lageveen <koen.lageveen@me.com>
2018-04-10 21:41:02 +0200

    reduce cruft

--- linter.py
+++ linter.py
@@ -1,25 +1,12 @@
-#
-# linter.py
-# Linter for SublimeLinter3, a code checking framework for Sublime Text 3
-#
-# Written by Aparajita Fishman
-# Copyright (c) 2015-2017 The SublimeLinter Community
-# Copyright (c) 2013-2014 Aparajita Fishman
-#
-# License: MIT
-#
-
-"""This module exports the HtmlTidy plugin class."""
-
 from SublimeLinter.lint import Linter, util
 
 
 class HtmlTidy(Linter):
-    """Provides an interface to tidy."""
-
-    syntax = 'html'
     regex = r'^line (?P<line>\d+) column (?P<col>\d+) - (?:(?P<error>Error)|(?P<warning>Warning)): (?P<message>.+)'
     error_stream = util.STREAM_STDERR
+    defaults = {
+        'selector': 'text.html.basic'
+    }
 
     def cmd(self):
         """Return a tuple with the command line to execute."""
